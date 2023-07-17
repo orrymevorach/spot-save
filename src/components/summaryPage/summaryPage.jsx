bfce710018befef17f31be3829f8e5263a1a6d62
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-07-17 13:49:46 -0400

    mobile polish

--- src/components/summaryPage/summaryPage.jsx
+++ src/components/summaryPage/summaryPage.jsx
@@ -28,7 +28,6 @@ export default function SummaryPage() {
     }
   }, [stageQuery, dispatch, actions]);
 
-  return <Loader isDotted />;
   if (isUserDataLoading || !user) return <Loader isDotted />;
 
   const cabinData = {
