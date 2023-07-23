0a1510677bf70f1dc23f366d5aebbdde0e0f6792
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-07-23 15:22:15 -0400

    cabin selection instructions

--- src/components/cabinSelectionPage/cabinSelectionContainer/cabinSelectionContainer.jsx
+++ src/components/cabinSelectionPage/cabinSelectionContainer/cabinSelectionContainer.jsx
@@ -3,14 +3,13 @@ import Units from '../units';
 import { useCabinSelection } from '@/context/cabin-selection-context';
 import { useCabinAndUnitData } from '@/context/cabin-and-unit-data-context';
 import Filters from '../filters/filters';
-import UnitLinks from '../unitLinks/unitLinks';
 import styles from './cabinSelectionContainer.module.scss';
 import Loader from '@/components/shared/loader/loader';
 import Button from '@/components/shared/button/button';
 import { useRef, useState } from 'react';
 import useShowOnScroll from '@/hooks/useShowOnScroll';
-import { useWindowSize } from '@/context/window-size-context';
 import MapOfCamp from '../mapOfCamp/mapOfCamp';
+import UnitDescriptions from '../unitDescriptions';
 
 export default function CabinSelectionContainer() {
   const [showBackToTopButton, setShowBackToTopButton] = useState(false);
@@ -18,7 +17,6 @@ export default function CabinSelectionContainer() {
   const { isLoading, units } = useCabinAndUnitData();
   const headerRef = useRef();
   useShowOnScroll({ ref: headerRef, setIsShowing: setShowBackToTopButton });
-  const { isMobile } = useWindowSize();
 
   if (isLoading || !units.length) return <Loader isDotted />;
 
@@ -28,28 +26,9 @@ export default function CabinSelectionContainer() {
     <>
       <div className={styles.outerContainer}>
         <div className={styles.headerContainer} ref={headerRef}>
-          {/* <p>Welcome to the Highlands Music Festival Reservation Platform:</p>
-          <ul>
-            <li>
-              At Highlands, we have 6 units. A unit is a village that consists
-              of 12+ cabins.
-            </li>
-            <li>
-              Each unit has its own washhouse, which has showers and washroom
-              for communal use.
-            </li>
-            <li>
-              The time is takes to walk from end-to-end in a single unit ranges
-              from 2-4 minutes. Cabins are scattered differently in each unit,
-              and are usually about 10 feet away from the closest cabin.
-            </li>
-          </ul>
-          <ol>
-            <li>hey</li>
-          </ol> */}
+          <UnitDescriptions />
           <Filters />
           <MapOfCamp />
-          {!isMobile && <UnitLinks />}
         </div>
       </div>
       <Units />
---
+++ src/components/cabinSelectionPage/unitDescriptions/unitDescriptions.jsx
@@ -0,0 +1,96 @@
+import Takeover from '@/components/shared/takeover';
+import styles from './unitDescriptions.module.scss';
+import { useState } from 'react';
+
+const unitDescriptions = [
+  {
+    unit: 'Colour Unit',
+    description:
+      'With cabin names based on the colours of the rainbow, the Colour Unit is close by to the Main Stage, Omni Stage, and the Dining Hall. Cabins in the Colour Unit have washrooms on the porches of the cabin and shared shower facilities nearby!',
+    anchor: 'Colours',
+  },
+  {
+    unit: 'Comic Unit',
+    description:
+      'With cabin names based on comic characters, the Comic Unit is a short walk away from the Amphitheatre stage and the beautiful waterfront. Cabins in the Comic Unit have washrooms on the porches of the cabins and shared shower facilities nearby!',
+    anchor: 'Comics',
+  },
+  {
+    unit: 'Zodiac Unit',
+    description:
+      'With cabin names based on the Astrological Star, the Zodiac Unit is close by to the Amphitheatre Stage and the beautiful waterfront. Shared washhouses, with stalls, sinks, and showers are a short walk away from the cabins in the Zodiac Unit! ',
+    anchor: 'Zodiacs',
+  },
+  {
+    unit: 'Seeker Unit',
+    description:
+      'With cabin names not related to the unit name, the Seeker Unit is a short walk away from the Main Stage, Omni Stage, and the Dining Hall. Shared washhouses, with stalls, sinks, and showers are a short walk away from the cabins in the Seeker Unit!',
+    anchor: 'Seekers',
+  },
+];
+
+const UnitsDescriptionTakeover = ({ setShowUnitDescriptions }) => {
+  return (
+    <Takeover handleClose={() => setShowUnitDescriptions(false)}>
+      <ul className={styles.instructions}>
+        {unitDescriptions.map(({ unit, description, anchor }) => {
+          return (
+            <li key={unit} className={styles.listItem}>
+              <p className={styles.unitName}>The {unit}</p>
+              <p className={styles.description}>{description}</p>
+              <p className={styles.clickHere}>
+                <a
+                  onClick={() => setShowUnitDescriptions(false)}
+                  href={`#${anchor}`}
+                  className={styles.anchor}
+                >
+                  Click here
+                </a>{' '}
+                to reserve a spot in the {unit}
+              </p>
+            </li>
+          );
+        })}
+        <li className={styles.listItem}></li>
+        <li className={styles.listItem}></li>
+        <li className={styles.listItem}></li>
+      </ul>
+    </Takeover>
+  );
+};
+
+export default function UnitDescriptions() {
+  const [showUnitDescriptions, setShowUnitDescriptions] = useState(false);
+  return (
+    <>
+      {showUnitDescriptions && (
+        <UnitsDescriptionTakeover
+          setShowUnitDescriptions={setShowUnitDescriptions}
+        />
+      )}
+      <div className={styles.textContainer}>
+        <p className={styles.lineOne}>
+          Welcome to the Highlands Music Festival Reservation Platform!
+        </p>
+        <p className={styles.text}>
+          At Highlands, the small villages of cabins around camp are called
+          Units.
+        </p>
+        <p className={styles.text}>
+          Below you will see available cabins within each unit. Please follow
+          the prompts to select the cabin you desire to book, and eventually a
+          bed within a cabin!
+        </p>
+        <p className={styles.text}>
+          To read more about each unit,{' '}
+          <span
+            onClick={() => setShowUnitDescriptions(!showUnitDescriptions)}
+            className={styles.anchor}
+          >
+            click here.
+          </span>
+        </p>
+      </div>
+    </>
+  );
+}
