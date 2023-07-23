67c80022ec0b37642eb54b0416c274102608b39a
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-07-23 11:21:41 -0400

    re-add apollo env variables

--- src/graphql/apollo-config.js
+++ src/graphql/apollo-config.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api.baseql.com/airtable/graphql/appsVLTJBkSew9M4y',
+  uri: `https://api.baseql.com/airtable/graphql/${process.env.NEXT_PUBLIC_BASEQL_APP_ID}`,
   cache: new InMemoryCache(),
   headers: {
-    authorization: `Bearer Y2MxZTUzNTAtNmNmYS00ZmIxLTgyY2ItYzNjY2JjYWFlNDg0`,
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_BASEQL_ACCESS_TOKEN}`,
   },
 });
