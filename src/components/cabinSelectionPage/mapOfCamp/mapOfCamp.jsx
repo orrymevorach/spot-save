c0688bf34a09b5d6bb203234d0758c56e0ec8830
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-07-25 21:38:39 -0400

    add link to map of camp takeover

--- src/components/cabinSelectionPage/mapOfCamp/mapOfCamp.jsx
+++ src/components/cabinSelectionPage/mapOfCamp/mapOfCamp.jsx
@@ -1,13 +1,68 @@
 import Takeover from '@/components/shared/takeover/takeover';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './mapOfCamp.module.scss';
 import fullCamp from 'public/fullCamp.jpg';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
+
+const MapOfCampTakeover = ({ setShowMapTakeover }) => {
+  const [isZoom, setIsZoom] = useState(false);
+  const router = useRouter();
+
+  // Add query param when takeover opens
+  useEffect(() => {
+    router.push(
+      {
+        query: {
+          showMap: true,
+        },
+      },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+    // Remove query param when takeover closes
+    return () => {
+      router.push(
+        {
+          query: {},
+        },
+        undefined,
+        {
+          shallow: true,
+        }
+      );
+    };
+  }, []);
+
+  return (
+    <Takeover
+      handleClose={() => setShowMapTakeover(false)}
+      modalClassNames={styles.takeover}
+    >
+      <Image
+        src={fullCamp}
+        alt=""
+        className={clsx(styles.map, isZoom && styles.isZoom)}
+        onClick={() => setIsZoom(!isZoom)}
+      />
+    </Takeover>
+  );
+};
 
 export default function MapOfCamp() {
   const [showMapTakeover, setShowMapTakeover] = useState(false);
-  const [isZoom, setIsZoom] = useState(false);
+  const router = useRouter();
+
+  // Show map takeover if showMap query param is true
+  useEffect(() => {
+    if (router?.query?.showMap === 'true') {
+      setShowMapTakeover(true);
+    }
+  }, [router, setShowMapTakeover]);
+
   return (
     <>
       <p onClick={() => setShowMapTakeover(true)} className={styles.text}>
@@ -15,17 +70,7 @@ export default function MapOfCamp() {
         grounds.
       </p>
       {showMapTakeover && (
-        <Takeover
-          handleClose={() => setShowMapTakeover(false)}
-          modalClassNames={styles.takeover}
-        >
-          <Image
-            src={fullCamp}
-            alt=""
-            className={clsx(styles.map, isZoom && styles.isZoom)}
-            onClick={() => setIsZoom(!isZoom)}
-          />
-        </Takeover>
+        <MapOfCampTakeover setShowMapTakeover={setShowMapTakeover} />
       )}
     </>
   );
